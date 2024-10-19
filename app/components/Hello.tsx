2daf9622d9b030ac0010c250e6b3e3997bb4e225
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-10-19 22:29:49 +0200

    update

--- app/components/Hello.tsx
+++ app/components/Hello.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image'
 
-const greeting: string = "Hello. I'm Marcin"
-
 export default function Hello() {
 	return (
 		<section className='flex w-full max-w-[355px] md:max-w-[728px] xl:max-w-[1040px] mx-auto gap-[20px]'>
@@ -13,7 +11,7 @@ export default function Hello() {
 				className='hidden md:block'
 			/>
 			<h1 className='flex flex-col justify-start gap-[5px]'>
-				<span className='text-2xl'>{greeting}</span>
+				<span className='text-2xl'>Hello. I&apos;m Marcin</span>
 				<span className='text-sm md:text-base xl:text-lg'>
 					A freelance Motion Designer & Illustrator based in Madrid / Spain.
 				</span>
