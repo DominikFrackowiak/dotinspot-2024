69e304f4fff017440ef63babe536263218141781
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-10-19 22:42:45 +0200

    update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import EmailLink from "./EmailLink"
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center max-w-[1200px] mx-auto h-20 gap-[20px]">
+    <footer className="flex justify-center items-center max-w-[1200px] mx-auto h-20 gap-[5px]">
      <Image src='/doticon.jpg' width={60} height={60} alt='illustration' className="block md:hidden"/> <EmailLink/>
     </footer>
   )
